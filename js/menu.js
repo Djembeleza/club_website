2b67cf452932b4de0e22614f60631f67bfbf4d89
Djembeleza <lkaola11@gmail.com>
2020-07-15 06:27:01 +0200

    Final page

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@
 const mainNav = document.getElementsByClassName('main-nav');
 const openBtn = document.getElementsByClassName('open');
 const closeBtn = document.getElementsByClassName('close');
+const clubTitle = document.getElementsByClassName('club-title');
 
 
 
@@ -9,13 +10,17 @@ const closeBtn = document.getElementsByClassName('close');
 openBtn[0].addEventListener('click', function () {
     mainNav[0].style.width = '100vw';
     openBtn[0].style.display = 'none';
+    clubTitle[0].style.position = 'absolute';
+    // clubTitle[0].style.top = '-3rem';
+
 
 });
 
 closeBtn[0].addEventListener('click', function () {
     mainNav[0].style.width = '0';
+
     setTimeout(function () {
         openBtn[0].style.display = 'inline-block';
-    }, 800)
+    }, 800);
 
 });
